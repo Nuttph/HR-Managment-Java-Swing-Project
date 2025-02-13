022f3da92028d04ebce6f6a8497ac45fe2fc5cb7
Nuttaphon <nathn2420@gmail.com>
2025-02-13 20:01:35 +0700

    update

--- src/Test/test1.java
+++ src/Test/test1.java
@@ -3,5 +3,7 @@ package Test;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("Update System");
+        System.out.println("Update System");
+        System.out.println("Update System");
     }
 }
